e3897281b26067861a936a22fee7b4b91236fb7a
ilg2005 <igoldfarb10@gmail.com>
2022-02-26 16:39:33 +0300

    текущее состояние

--- src/components/AppAvatar.vue
+++ src/components/AppAvatar.vue
@@ -12,5 +12,14 @@ export default {
 </script>
 
 <style scoped>
+.avatar {
+  display: flex;
+  justify-content: center;
+}
 
+.avatar img {
+  width: 150px;
+  height: auto;
+  border-radius: 50%;
+}
 </style>
