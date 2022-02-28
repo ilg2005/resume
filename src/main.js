71089b45e08ffcbae68a72873d9302182484051d
ilg2005 <igoldfarb10@gmail.com>
2022-02-28 15:23:04 +0300

    новая БД в firebase

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './theme.css'
 
-export const firebaseUrl = 'https://vue-demo-deploy-7673c-default-rtdb.asia-southeast1.firebasedatabase.app/fields.json'
+export const firebaseUrl = 'https://vue-resume-deploy-1946a-default-rtdb.asia-southeast1.firebasedatabase.app/fields.json'
 
 createApp(App).mount('#app')
 
